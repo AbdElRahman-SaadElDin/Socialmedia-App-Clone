0d729dccd97244184ceaef63919f2c3512df7e55
AbdElRahman-SaadElDin <131001698+AbdElRahman-SaadElDin@users.noreply.github.com>
2025-05-22 07:06:46 +0300

    Simulate login save to LocalStorage

---
+++ assets/js/redirect.js
@@ -0,0 +1,7 @@
+// if the user is already logged in, redirect to the home page
+const savedUser = localStorage.getItem( "user" );
+
+if ( savedUser )
+{
+    window.location.href = "pages/home.html";
+}
